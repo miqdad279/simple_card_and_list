f674247dd7c77a870d33690451cc12943e4ec4b3
miqdad279 <miqdadabdillah279@gmail.com>
2023-06-07 11:20:59 +0700

    first commit

---
+++ lib/mahasiswa.dart
@@ -0,0 +1,15 @@
+class Mahasiswa {
+  String name;
+  String nim;
+  String tahun;
+  String prodi;
+  String profileImg;
+
+  Mahasiswa({
+    required this.name,
+    required this.nim,
+    required this.tahun,
+    required this.prodi,
+    required this.profileImg,
+  });
+}
---
+++ lib/person.dart
@@ -0,0 +1,11 @@
+class Person {
+  String name;
+  String profileImg;
+  String bio;
+
+  Person({
+    required this.name,
+    required this.profileImg,
+    required this.bio,
+  });
+}
